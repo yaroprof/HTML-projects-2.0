68c53235af488397757cff93c65cdda1ad78d965
Yaroslav <yaroslavs.tech@gmail.com>
2025-10-06 21:40:03 +0300

    feat: add cript and classes- show-remove menu

--- 05-watchStore-app/assets/js/main.js
+++ 05-watchStore-app/assets/js/main.js
@@ -1,7 +1,23 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
 
 
 /*=============== SWIPER WACTHES ===============*/
